69504550281887d12f0e55cc0101ff090dbbd9f4
lanceccraig <lancecraig513@outlook.com>
2021-01-09 11:15:36 -0500

    Setup project references and package info

---
+++ tests/TestingSuppressions.cs
@@ -0,0 +1,26 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Design",
+    "CA1034:Nested types should not be visible",
+    Justification = "Nested classes are used for methods in unit test classes.")]
+[assembly: SuppressMessage(
+    "Design",
+    "CA1062:Validate arguments of public methods",
+    Justification = "Test methods are not under the same constraints as typical public methods.")]
+[assembly: SuppressMessage(
+    "Naming",
+    "CA1707:Identifiers should not contain underscores",
+    Justification = "Allowed in test method names.")]
+[assembly: SuppressMessage(
+    "Reliability",
+    "CA2007:Consider calling ConfigureAwait on the awaited task",
+    Justification = "Test methods will not be called outside of the scope of the project.")]
+[assembly: SuppressMessage(
+    "StyleCop.CSharp.ReadabilityRules",
+    "SA1118:Parameter should not span multiple lines",
+    Justification = "This situation is hard to avoid with complex theory data.")]
